dd961f78ff895a05faab8cd3240818cf11c8fcf8
e.lezov <e.lezov@tinkoff.ru>
2023-02-27 16:29:14 +0300

    add watermark

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -9,37 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-            VStack(spacing: 10) {
-                CapsuleText(text: "First")
-                    .foregroundColor(.white)
-                CapsuleText(text: "Second")
-                    .foregroundColor(.red)
-                Text("Hello World")
-                    .modifier(Title())
-            }
-        }
-}
-
-struct CapsuleText: View {
-    var text: String
-
-    var body: some View {
-        Text(text)
-            .font(.largeTitle)
-            .padding()
-            .background(.blue)
-            .clipShape(Capsule())
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
     }
 }
 
-struct Title: ViewModifier {
+struct Watermark: ViewModifier {
+    var text: String
+
     func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-            .background(.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
     }
 }
 
